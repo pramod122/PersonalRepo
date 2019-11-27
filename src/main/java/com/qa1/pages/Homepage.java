572d71a2b79a9c502c3d295550794f706844fdf6
pummy123 <43612856+pramod122@users.noreply.github.com>
2019-11-27 22:15:40 +0000

    Update Homepage.java

--- src/main/java/com/qa1/pages/Homepage.java
+++ src/main/java/com/qa1/pages/Homepage.java
@@ -4,5 +4,6 @@ public class Homepage {
   
   public void homepage12(){
   System.out.println("This is amended by Pramod");
+    System.out.println(" the code has been amended by pramod for re pull");
   }
 }
